f1bb5d66fb1b178fdb7a01898ce506874beb7808
Mohamed Ben Rhaiem <102826267+Mohamed-BenRhaiem@users.noreply.github.com>
2023-02-04 12:56:21 +0100

    adding links to the icons

--- MediaIcons.js
+++ MediaIcons.js
@@ -5,10 +5,10 @@ function MediaIcons() {
   return (
     <>
     <div id='socialIcons'>
-        <Facebook className='social-icon'/>
-        <Twitter className='social-icon'/>
-        <GitHub className='social-icon'/>
-        <Linkedin className='social-icon'/>
+        <a className='social-link' href='https://www.facebook.com/profile.php?id=100009527520303'><Facebook className='social-icon'/></a>
+        <a className='social-link' href='https://twitter.com/Mohamed_BRhaiem'><Twitter className='social-icon'/></a>
+        <a className='social-link' href='https://github.com/Mohamed-BenRhaiem'> <GitHub className='social-icon'/></a>
+        <a className='social-link' href='https://www.linkedin.com/in/mohamed-ben-rhaiem'><Linkedin className='social-icon'/></a>
     </div>
     </>
     
